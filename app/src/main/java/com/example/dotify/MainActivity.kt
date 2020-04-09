af40533c9e422557178eddee72b53a6cac8859e5
Khoa Nguyen Luong <khoa.luong@yahoo.com.vn>
2020-04-09 10:01:15 -0700

    set lambda functions for event listeners

--- app/src/main/java/com/example/dotify/MainActivity.kt
+++ app/src/main/java/com/example/dotify/MainActivity.kt
@@ -6,6 +6,7 @@ import android.util.Log
 import android.view.View
 import android.widget.TextView
 import android.widget.Toast
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
     private var viewCount = 0
@@ -14,27 +15,44 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         populateViewCount()
+
+        ibPrev.setOnClickListener {
+            v: View -> fnPrevSong(v)
+        }
+        ibPlay.setOnClickListener {
+            v: View -> fnPlaySong(v)
+        }
+        ibNext.setOnClickListener {
+            v: View -> fnNextSong(v)
+        }
+        btnChangeUser.setOnClickListener {
+            v: View -> fnChangeUser(v)
+        }
     }
 
-    fun fnPlaySong(view: View) {
+    private fun fnPlaySong(view: View) {
         Log.i("dotify", "play pressed")
         viewCount++
         populateViewCount()
     }
 
-    fun fnPrevSong(view: View) {
+    private fun fnPrevSong(view: View) {
         Log.i("dotify", "prev pressed")
         Toast.makeText(this, "Skipping to previous track", Toast.LENGTH_SHORT).show()
     }
 
-    fun fnNextSong(view: View) {
+    private fun fnNextSong(view: View) {
         Log.i("dotify", "next pressed")
         Toast.makeText(this, "Skipping to next track", Toast.LENGTH_SHORT).show()
     }
 
+    private fun fnChangeUser(view: View) {
+        Log.i("dotify", "change user")
+
+    }
+
     //Helper functions
     private fun populateViewCount() {
-        val tvViewCount = findViewById<TextView>(R.id.tvViewCount)
         tvViewCount.text = "$viewCount plays"
     }
 }
