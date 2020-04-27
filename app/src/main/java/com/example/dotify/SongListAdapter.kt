a1c568738637e78b092b7243c06ff8f3d726625c
Khoa Nguyen Luong <khoa.luong@yahoo.com.vn>
2020-04-26 23:34:35 -0700

    finish adapter class

--- app/src/main/java/com/example/dotify/SongListAdapter.kt
+++ app/src/main/java/com/example/dotify/SongListAdapter.kt
@@ -1,5 +1,36 @@
 package com.example.dotify
 
-class SongListAdapter {
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.ImageView
+import android.widget.TextView
+import androidx.recyclerview.widget.RecyclerView
+import com.ericchee.songdataprovider.Song
+
+class SongListAdapter(private val listOfSongs: List<Song>): RecyclerView.Adapter<SongListAdapter.SongViewHolder>() {
+
+    class SongViewHolder(itemView: View): RecyclerView.ViewHolder(itemView) {
+        private val tvSongName by lazy { itemView.findViewById<TextView>(R.id.tvSongName) }
+        private val tvArtist by lazy {itemView.findViewById<TextView>(R.id.tvArtist)}
+        private val ivAlbumCover by lazy {itemView.findViewById<ImageView>(R.id.ivAlbumCover)}
+        fun bind(song: Song) {
+            tvSongName.text = song.title;
+            tvArtist.text = song.artist;
+            ivAlbumCover.setImageResource(song.smallImageID)
+        }
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): SongViewHolder {
+        val view = LayoutInflater.from(parent.context).inflate(R.layout.item_music, parent, false)
+        return SongViewHolder(view)
+    }
+
+    override fun getItemCount(): Int = listOfSongs.size
+
+    override fun onBindViewHolder(holder: SongViewHolder, position: Int) {
+        val song = listOfSongs[position]
+        holder.bind(song)
+    }
 
 }
